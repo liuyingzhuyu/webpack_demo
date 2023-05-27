6cd92d441c5972258a9246c481a0c1cb5d6fd652
lucy <13161166589@163.com>
2023-05-27 18:20:02 +0800

    12.压缩html

---
+++ 12.压缩html/webpack.config.js
@@ -0,0 +1,30 @@
+
+const { options } = require('less')
+const { resolve } = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: 'built.js',
+        path: resolve(__dirname, 'build'),// __dirname 当前目录绝对路径
+    },
+    // loader配置
+    module: {
+    },  
+    // plugin（插件）配置
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './src/index.html',
+            // 压缩html
+            minify:{
+                //移除空格
+                collapseWhitespace: true,
+                //删除注释
+                removeComments: true,
+            }
+        })
+    ],
+    // mode: 'development',
+    // 生产环境下会自动压缩js代码
+    mode: 'production'
+}
